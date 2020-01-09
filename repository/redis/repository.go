ce0dd4add17d0910724ec24f35bcf9e1e89829ae
Leonardo Souza <leogsouza@gmail.com>
2020-01-09 17:11:22 -0300

    Implementing Repositories

--- repository/redis/repository.go
+++ repository/redis/repository.go
@@ -1,6 +1,9 @@
 package redis
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/go-redis/redis"
 	"github.com/leogsouza/url-shortener/shortener"
 	"github.com/pkg/errors"
@@ -30,5 +33,45 @@ func NewRedisRepository(redisURL string) (shortener.RedirectRepository, error) {
 		return nil, errors.Wrap(err, "repository.NewRedisRepository")
 	}
 	repo.client = client
-	return nil, nil
+	return repo, nil
+}
+
+func (r *redisRepository) generateKey(code string) string {
+	return fmt.Sprintf("redirect:%s", code)
+}
+
+func (r *redisRepository) Find(code string) (*shortener.Redirect, error) {
+	redirect := &shortener.Redirect{}
+	key := r.generateKey(code)
+	data, err := r.client.HGetAll(key).Result()
+	if err != nil {
+		return nil, errors.Wrap(err, "repository.Redirect.Find")
+	}
+
+	if len(data) == 0 {
+		return nil, errors.Wrap(shortener.ErrRedirectNotFound, "repository.Redirect.Find")
+	}
+	createdAt, err := strconv.ParseInt(data["created_at"], 10, 64)
+	if err != nil {
+		return nil, errors.Wrap(err, "repository.Redirect.Find")
+	}
+	redirect.Code = data["code"]
+	redirect.URL = data["url"]
+	redirect.CreatedAt = createdAt
+
+	return redirect, nil
+}
+
+func (r *redisRepository) Store(redirect *shortener.Redirect) error {
+	key := r.generateKey(redirect.Code)
+	data := map[string]interface{}{
+		"code":       redirect.Code,
+		"url":        redirect.URL,
+		"created_at": redirect.CreatedAt,
+	}
+	_, err := r.client.HMSet(key, data).Result()
+	if err != nil {
+		return errors.Wrap(err, "repository.Redirect.Store")
+	}
+	return nil
 }
